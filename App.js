f7bb2d69c1e65ea664c572935407b920ccd58b8d
Anthony Ellis <aellisnola@gmail.com>
2020-03-15 11:14:37 -0500

    Set up Navigation to be implemented later. Currently only Index screen is showing.

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello, World!</Text>
-    </View>
-  );
-}
+import IndexScreen from './src/screens/IndexScreen'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const navigator = createStackNavigator({
+  Index: IndexScreen
+}, {
+  initialRouteName: 'Index',
+  defaultNavigationOptions: {
+    title: 'Blogs'
+  }
+})
+
+export default createAppContainer(navigator)
