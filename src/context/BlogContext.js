fab3e912f8232c2bf0b9cc940c50885cd9f78cc3
Anthony Ellis <aellisnola@gmail.com>
2020-03-17 14:08:04 -0500

    Added state to Provider. Created addBlogPost functionality.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,12 +1,18 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 
 /* BlogContext pipes the data from the overarching Blog Post Provider to children components */
 const BlogContext = createContext()
 
 /* BlogProvider is the component where the data will be stored */
 export const BlogProvider = ({ children }) => {
+    const [blogPosts, setBlogPosts] = useState([])
+
+    const addBlogPost = () => {
+        setBlogPosts([...blogPosts, {title: `Blog Post #${blogPosts.length + 1}`}])
+    }
+
     return (
-        <BlogContext.Provider value={"Funneling data from overarching Provider to child screen component!"}>
+        <BlogContext.Provider value={{ data: blogPosts, addBlogPost}}>
             {children}
         </BlogContext.Provider>
     )
