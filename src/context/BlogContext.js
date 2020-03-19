5c6786e283f799253290470d7003fc548b616021
Anthony Ellis <aellisnola@gmail.com>
2020-03-19 12:06:16 -0500

    Added some light styling to Index Screen. Added trash icon to each post. Wired up delete functionality.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,20 +3,31 @@ import createDataContext from './createDataContext'
 const blogReducer = (state, action) => {
     switch (action.type) {
         case 'add_blogpost':
-            return [...state, { title: `Blog Post #${state.length + 1}` }]
+            // return all of the state, then add into it the new post
+            return [...state, { id: Math.floor(Math.random() * 99999), title: `Blog Post #${state.length + 1}` }]
+        case 'delete_blogpost':
+            // iterate through state array. If true is returned, that item is returned in a new array.
+            // Every post that doesn't have the id we specify will continue to exist in that new array that is being returned
+            return state.filter(blogPost => blogPost.id !== action.payload)
         default:
             return state
     }
 }
 
-const addBlogPost = (dispatch) => {
+const addBlogPost = dispatch => {
     return () => {
         dispatch({ type: 'add_blogpost' })
     }
 }
 
+const deleteBlogPost = dispatch => {
+    return (id) => {
+        dispatch({ type: 'delete_blogpost', payload: id })
+    }
+}
+
 export const { Context, Provider } = createDataContext(
     blogReducer,
-    { addBlogPost },
+    { addBlogPost, deleteBlogPost },
     []
 )
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,20 +1,25 @@
 import React, { useContext } from 'react'
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native'
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native'
 import { Context } from '../context/BlogContext'
+import { Feather } from '@expo/vector-icons'
 
 const IndexScreen = () => {
-    const { state, addBlogPost } = useContext(Context)
+    const { state, addBlogPost, deleteBlogPost } = useContext(Context)
 
     return (
         <View>
-            <Text>Index Screen</Text>
             <Button title='Add Post' onPress={addBlogPost} />
             <FlatList
                 data={state}
-                keyExtractor={blogPost => blogPost.title}
+                keyExtractor={blogPost => blogPost.id}
                 renderItem={({ item }) => {
                     return (
-                        <Text>{item.title}</Text>
+                        <View style={styles.row}>
+                            <Text style={styles.title}>{item.title}</Text>
+                            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                <Feather name="trash" style={styles.icon} />
+                            </TouchableOpacity>
+                        </View>
                     )
                 }}
             />
@@ -22,6 +27,26 @@ const IndexScreen = () => {
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    row: {
+        // Everything on same line
+        flexDirection: 'row',
+        // Seperate title and trash icon
+        justifyContent: 'space-between',
+        // Vertical breathing room between posts
+        paddingVertical: 20,
+        // Horizontal breathing room from screen edge
+        paddingHorizontal: 10,
+        // Post seperating border
+        borderBottomWidth: 1,
+        borderColor: 'gray'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
+})
 
 export default IndexScreen
